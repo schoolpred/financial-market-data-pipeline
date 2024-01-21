fd5753b8c8ff292b2aa4c40599542cbe462bcf4d
Truong Le <lequangtruong1108@gmail.com>
2024-01-21 15:24:51 +0700

    add new get data func

--- etl/extract.py
+++ etl/extract.py
@@ -25,3 +25,35 @@ class get_data_from_finnhub():
         endpoint = "/stock/symbol"
         params = {'exchange': exchange_code}
         return self._make_request(endpoint, params)
+
+    def get_company_profile(self, symbol):
+        """
+        Get general information of company
+        """
+        endpoint = "/stock/profile2"
+        params = {'symbol': symbol}
+        return self._make_request(endpoint, params)
+
+    def get_basic_basic_financials(self, symbols, metric='all'):
+        """
+        Get basic financials given symbol such as margin, P/E ratio, 52-week high/low etc.
+        """
+        endpoint = "/stock/metric"
+        params = {'symbol': symbols, 'metric' : metric}
+        return self._make_request(endpoint, params)
+
+    def get_stock_price(self, symbol):
+        """
+        Get stock price for a given symbol
+        """
+        endpoint = "/quote"
+        params = {'symbol': symbol}
+        return self._make_request(endpoint, params)
+    
+    def get_company_news(self, symbol, from_date, to_date):
+        """
+        Get company news for a given symbol
+        """
+        endpoint = "/company-news"
+        params = {'symbol': symbol, 'from': from_date, 'to': to_date}
+        return self._make_request(endpoint, params)
